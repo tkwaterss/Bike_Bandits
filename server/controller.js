dea341c867eb02bf3da77a11b6ad1acb589fc151
Tobin Waters <tkwaterss@gmail.com>
2023-03-06 16:59:53 -0700

    edits to get server up

--- server/controller.js
+++ server/controller.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const {CONNECTION_STRING} = process.env;
+// require('dotenv').config();
+// const {CONNECTION_STRING} = process.env;
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
-  })
+// const sequelize = new Sequelize(CONNECTION_STRING, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     }
+//   })
 
-module.exports = {
-    //Server functions
-}
+// module.exports = {
+//     loadPage: (req, res) => {
+//         res.status(200).sendFile(path.join(__dirname, '../public/home.html'))
+//     }
+// }
